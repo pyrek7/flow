429eb00437f1610c305a993440e9b84abf3e2519
pfiderek <fiderek@gmail.com>
2023-01-12 10:12:57 +0100

    first init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
